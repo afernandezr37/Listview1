03d86dbf6e06c4f743300931d0aa2efa2f24ce68
Aaron <afernandezr@cifpfbmoll.eu>
2022-02-03 17:00:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/listview1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.listview1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.SortedListAdapterCallback;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+    TextView selection;
+    private static final String[] items={"lorem", "ipsum", "dolor",
+            "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus"};
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        setListAdapter(new IconicAdapter());
+        selection=(TextView)findViewById(R.id.selection);
+    }
+    public void onListItemClick(ListView parent, View v,
+                                int position, long id) {
+        selection.setText(items[position]);
+    }
+    class IconicAdapter extends ArrayAdapter<String> {
+        IconicAdapter() {
+            super(MainActivity.this, R.layout.row, R.id.label, items);
+        }
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            View row=super.getView(position, convertView, parent);
+            ImageView icon=(ImageView)row.findViewById(R.id.icon);
+            if (items[position].length()>4) {
+                icon.setImageResource(R.drawable.ic_baseline_cancel_24);
+            }
+            else {
+                icon.setImageResource(R.drawable.ic_baseline_check_24);
+            }
+            return(row);
+        }
+    }
+}
